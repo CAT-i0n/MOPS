66e392464af345b4d51583503a2f35dfc7cd31fd
CAT-i0n <rudman.iv.o@gmail.com>
2022-10-20 13:26:01 +0300

    Add tree class

--- tree/main.py
+++ tree/main.py
@@ -3,27 +3,26 @@ from antlr4 import *
 from Python3Lexer import Python3Lexer
 from Python3Parser import Python3Parser
 
-
+class algTree:
+    def __init__(self):
+        childs = []
  
-def visitor(a):
-    if a.__class__.__name__ == 'TerminalNodeImpl':
-        if a.getText()=='<EOF>':
-            print("end")
+    def visit(self, a):
+        if a.__class__.__name__ == 'TerminalNodeImpl':
+            if a.getText()=='<EOF>':
+                print("end", end = "")
+                return
+            if a.getText()!='\n':
+                print("(",a.getText(),")", end = "")
             return
-        if a.getText()!='\n':
-            print("(",a.getText(),")")
-        if a.getText()=='<EOF>':
-            print("end")
-        return
-    else:
-        print('(', end = "")
-        #print(a.__class__.__name__, end = "")
-        for i in a.children:
-            visitor(i)
-        print(')', end = "")
-        return
-    
-
+        else:
+            print('(', end = "")
+            print(a.__class__.__name__, end = "")
+            for i in a.children:
+                self.visit(i)
+            print(')', end = "")
+            return
+     
 
 def main(argv):
     input_stream = FileStream(argv[1])
@@ -31,10 +30,11 @@ def main(argv):
     stream = CommonTokenStream(lexer)
     parser = Python3Parser(stream)
     tree = parser.file_input()
-    print(dir(tree))
-    visitor(tree)
+    walker = algTree()
+    walker.visit(tree)
+
+main([None, "test.py"])
 
 
-
-if __name__ == '__main__':
-    main(sys.argv)
+#if __name__ == '__main__':
+#    main(sys.argv)
