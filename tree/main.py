d4f98ff2b07c86307af045c23f3ce1d8133412ce
CAT-i0n <rudman.iv.o@gmail.com>
2022-10-20 13:47:23 +0300

    Fix tree

--- tree/main.py
+++ tree/main.py
@@ -4,24 +4,26 @@ from Python3Lexer import Python3Lexer
 from Python3Parser import Python3Parser
 
 
- 
-def visitor(a):
-    if a.__class__.__name__ == 'TerminalNodeImpl':
-        if a.getText()=='<EOF>':
-            print("end")
+class algTree:
+    def __init__(self):
+        childs = []
+    def visit(self, a):
+        if a.__class__.__name__ == 'TerminalNodeImpl':
+            if a.getText()=='<EOF>':
+                print("end", end = "")
+                return
+            if a.getText()!='\n':
+                print("(",a.getText(),")", end = "")
+            if a.getText()=='<EOF>':
+                print("end", end = "")
             return
-        if a.getText()!='\n':
-            print("(",a.getText(),")")
-        if a.getText()=='<EOF>':
-            print("end")
-        return
-    else:
-        print('(', end = "")
-        #print(a.__class__.__name__, end = "")
-        for i in a.children:
-            visitor(i)
-        print(')', end = "")
-        return
+        else:
+            print('(', end = "")
+            print(a.__class__.__name__, end = "")
+            for i in a.children:
+                self.visit(i)
+            print(')', end = "")
+            return
     
 
 
@@ -31,10 +33,12 @@ def main(argv):
     stream = CommonTokenStream(lexer)
     parser = Python3Parser(stream)
     tree = parser.file_input()
-    print(dir(tree))
-    visitor(tree)
+    walker = algTree()
+    walker.visit(tree)
+    
+
+main([None, 'test.py'])
 
 
-
-if __name__ == '__main__':
-    main(sys.argv)
+#if __name__ == '__main__':
+#    main(sys.argv)
