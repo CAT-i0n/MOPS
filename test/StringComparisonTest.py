818a68a3d7bd575c921914fcfb54149c19939eb0
CAT-i0n <rudman.iv.o@gmail.com>
2022-12-06 18:02:01 +0300

    replace testing

--- test/StringComparisonTest.py
+++ test/StringComparisonTest.py
@@ -1,4 +1,5 @@
-
+import sys
+sys.path.append('../')
 
 from alg.StringComparison import *
 
@@ -10,8 +11,8 @@ def mapText(text: str):
     return text
 
 if __name__ == "__main__":
-    test_text = getText('tree/test.py')
-    test2_text = getText('tree/test2.py')
+    test_text = getText('examples/test.py')
+    test2_text = getText('examples/test2.py')
     test_text = mapText(test_text)
     test2_text = mapText(test2_text)
     c = comparison(test_text, test2_text, 4, 2)
--- test/TokenTypeComparisonTest.py
+++ test/TokenTypeComparisonTest.py
@@ -1,9 +1,11 @@
+import sys
+sys.path.append('../')
 
 from alg.TokenTypeComparison import *
 
 
 if __name__ == "__main__":
-    test_tokens = getTokenTypes('tree/test.py')
-    test2_tokens = getTokenTypes('tree/test2.py')
+    test_tokens = getTokenTypes('examples/test.py')
+    test2_tokens = getTokenTypes('examples/test2.py')
     c = comparison(test_tokens, test2_tokens).similarity()
     print(c)
