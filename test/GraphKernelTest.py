60b8420e55435b51d0ac7f1db25ef22b0900f86c
CATi0n <73797488+CAT-i0n@users.noreply.github.com>
2022-12-08 00:14:47 +0300

    Fixes

--- test/GraphKernelTest.py
+++ test/GraphKernelTest.py
@@ -1,15 +1,15 @@
 import sys
 sys.path.append('../')
 #from com.load_project import prepare_projects
-from tree.Python3Builder import algTree
+from tree.TreeBuilder import SyntaxTree
 from alg.GraphKernelComparison import GraphKernelComparison,percent
 
 if __name__ == "__main__":
-    tree1 = algTree()
+    tree1 = SyntaxTree()
     project1_path = r'examples/test.py'
     tree1.root(project1_path)
 
-    tree2 = algTree()
+    tree2 = SyntaxTree()
     project2_path = r'examples/test2.py'
     tree2.root(project2_path)
     print(GraphKernelComparison( tree1, tree2))
