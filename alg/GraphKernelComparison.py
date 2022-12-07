8f7fa9e80c278596f4fb90075e55f3b3913e3686
Krachkovskiy Andrey <64749082+bersil11001@users.noreply.github.com>
2022-12-07 16:39:56 +0300

    add algo

---
+++ alg/GraphKernelComparison.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def add(queue,ls):
+    for el in ls:
+        queue.append(el)
+
+
+def GraphKernelComparison(tree1,tree2,measure=0):
+    if tree1.get_children()==[] or tree2.get_children()==[]:
+        return measure
+    queue1=deque()
+    queue2=deque()
+    s1=set()
+    a=1
+    add(queue1,[tree1])
+    while len(queue1)>0:
+        a=a+1
+        el1 =queue1.popleft()
+        add(queue1,el1.get_children())
+        add(queue2,[tree2])
+        while len(queue2)>0:
+            el2 = queue2.popleft()
+            if el1.get_label()==el2.get_label() and  (el1.get_label() not in s1):
+                measure=measure+1
+                s1.add(el2.get_label())
+            add(queue2,el2.get_children())
+    return measure
+
+
+def percent(tree1,tree2):
+    return GraphKernelComparison(tree1,tree2)/GraphKernelComparison(tree1,tree1)
