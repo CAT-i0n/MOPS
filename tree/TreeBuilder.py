fc9327ca11f8a053bfd192552c5f0ac44e8bf2f0
CATi0n <73797488+CAT-i0n@users.noreply.github.com>
2022-12-07 23:52:53 +0300

    Add tree simlification

--- tree/TreeBuilder.py
+++ tree/TreeBuilder.py
@@ -4,7 +4,7 @@ from tree.Python3Lexer import Python3Lexer
 from tree.Python3Parser import Python3Parser
 
 
-class algTree:
+class SyntaxTree:
     def __init__(self, ast = None):
         self.ast = ast
         self.childs = []
@@ -12,13 +12,15 @@ class algTree:
         self.data = ''
         self.size = 0
     
-    def root(self, datapath):
+    def root(self, datapath, simplify = True):
         input_stream = FileStream(datapath)
         lexer = Python3Lexer(input_stream)
         stream = CommonTokenStream(lexer)
         self.parser = Python3Parser(stream)
         self.ast = self.parser.file_input()
         self.size = self._build()
+        if simplify:
+            self.size -= self.simplify()
         
     def _build(self, rules = None):
         if not rules:
@@ -31,10 +33,23 @@ class algTree:
             local_size = len(self.ast.children)
             self.data_type = rules.ruleNames[self.ast.getRuleIndex()] 
             for i in self.ast.children:
-                child = algTree(i)
+                child = SyntaxTree(i)
                 local_size += child._build(rules)
                 self.childs.append(child)
             return local_size
+    
+    def simplify(self):
+        size = 0
+        for index, child in enumerate(self.childs):
+            if len(child.childs) == 1:
+                while len(self.childs[index].childs)==1:
+                    self.childs[index] = self.childs[index].childs[0]
+                    size += 1
+                size += self.childs[index].simplify()
+            else:
+                size += self.childs[index].simplify()
+        return size
+                
     
     def visit(self):
         if self.data_type == 'terminal':
