1a8669361e33f5ccd11baf5b584ee0aaa5265424
CATi0n <73797488+CAT-i0n@users.noreply.github.com>
2022-12-07 22:27:58 +0300

    Edit TreeComaprison

--- alg/TreeComparison.py
+++ alg/TreeComparison.py
@@ -1,8 +1,12 @@
 import zss
 
 def zcc_comparison(algTree, tree1, tree2):
-    return zss.simple_distance(
+    diff = zss.simple_distance(
         tree1, tree2, algTree.get_children, algTree.get_label)
+    dlen = abs(tree1.size - tree2.size)
+    absd = diff - dlen
+    return ((tree1.size - absd)/tree1.size,
+            (tree2.size - absd)/tree2.size)
 
 #change None with algorithm function
 def another_comparison(algTree, tree1, tree2):
---
+++ test/TreeComparisonTest.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('../')
+#from com.load_project import prepare_projects
+from tree.Python3Builder import algTree
+from alg.TreeComparison import zcc_comparison
+
+if __name__ == "__main__":
+    tree1 = algTree()
+    project1_path = r'examples/test.py'
+    tree1.root(project1_path)
+
+    tree2 = algTree()
+    project2_path = r'examples/test2.py'
+    tree2.root(project2_path)
+    print(zcc_comparison(algTree, tree1, tree2))
+    
